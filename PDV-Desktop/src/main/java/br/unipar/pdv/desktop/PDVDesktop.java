b24f6865f77d257263ba4ee4e3df8aab9025fb94
Vini123t <111025380+Vini123t@users.noreply.github.com>
2024-05-31 21:10:13 -0300

    Comit

---
+++ PDV-Desktop/src/main/java/br/unipar/pdv/desktop/PDVDesktop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.unipar.pdv.desktop;
+
+/**
+ *
+ * @author vinid
+ */
+public class PDVDesktop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
